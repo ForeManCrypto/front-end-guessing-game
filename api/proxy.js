f8d7c3469f329b84424a7c18e17f6e4c10021c40
CryptoBarnal <moneyforeman@gmail.com>
2025-04-15 21:06:44 -0700

    Add Vercel proxy for ShareLedger RPC

--- api/proxy.js
+++ api/proxy.js
@@ -1,37 +1,18 @@
-const fetch = require('node-fetch');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  if (req.headers.upgrade === 'websocket') {
-    createProxyMiddleware({
-      target: 'wss://rpc-testnet.shareri.ng',
-      ws: true,
-      changeOrigin: true,
-      pathRewrite: { '^/api/proxy': '' },
-      logLevel: 'debug',
-    })(req, res);
-  } else {
-    const url = 'https://rpc-testnet.shareri.ng' + req.url.replace('/api/proxy', '');
-    fetch(url, {
-      method: req.method,
-      headers: {
-        ...req.headers,
-        host: undefined,
-        origin: undefined,
-        referer: undefined,
-      },
-      body: req.method !== 'GET' && req.method !== 'HEAD' ? JSON.stringify(req.body) : undefined,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.status(response.status).json(data);
-      })
-      .catch((error) => {
-        console.error('Proxy error:', error);
-        res.status(500).json({ error: 'Failed to proxy request' });
-      });
-  }
+  createProxyMiddleware({
+    target: 'https://rpc-testnet.shareri.ng',
+    changeOrigin: true,
+    pathRewrite: { '^/api/proxy': '' },
+    onProxyRes: (proxyRes) => {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
+      proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    },
+    onError: (err, req, res) => {
+      console.error('Proxy error:', err);
+      res.status(500).send('Proxy error');
+    }
+  })(req, res);
 };
