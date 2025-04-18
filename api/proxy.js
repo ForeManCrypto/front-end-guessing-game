f1e9959d891d9c03b9ce95d6970b3e9c543e18c4
CryptoBarnal <moneyforeman@gmail.com>
2025-04-17 22:08:37 -0700

    Fix SyntaxError, 401, and WebSocket

--- api/proxy.js
+++ api/proxy.js
@@ -1,26 +1,19 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  console.log('Proxy request:', req.url, 'Method:', req.method);
-  const proxy = createProxyMiddleware({
-    target: 'http://35.199.47.86:26657',
-    //target: 'https://rpc-testnet.shareri.ng',
+  createProxyMiddleware({
+    target: 'https://rpc-testnet.shareri.ng',
     changeOrigin: true,
-    ws: true,
     pathRewrite: { '^/api/proxy': '' },
     onProxyRes: (proxyRes) => {
       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
       proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
       proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-      console.log('Proxy response headers:', proxyRes.headers);
-    },
-    onProxyReqWs: (proxyReq, req, socket) => {
-      console.log('WebSocket request:', req.url);
     },
     onError: (err, req, res) => {
       console.error('Proxy error:', err);
       res.status(500).send('Proxy error');
     }
-  });
-  proxy(req, res);
+  })(req, res);
 };
+
